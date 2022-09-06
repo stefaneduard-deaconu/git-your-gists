4598ccdaf6fb0ae6136ae9e5c004aaa28de5f87d
Stefan-Eduard Deaconu <stefan@stefandeaconu.com>
2022-09-07 02:05:45 +0300

    added navigation, favicon and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import styles from './App.module.css';
+import {Link} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <>
+            <nav className={styles.nav}>
+                <Link to={'/'}>Git Your Gists</Link>
+            </nav>
+
+            <main className={styles.main}>
+                {/* TODO implement the features here:*/}
+                <h1>Features on their way!</h1>
+            </main>
+
+            <footer className={styles.footer}>
+                <div className={styles.headline}>
+                    Everyday is the right day for one more mental victory - &copy; Ștefan - 2022
+                </div>
+                <div className={styles.column}>
+                    Resources used in building this site:
+                    <ul>
+                        <li><a href="https://www.flaticon.com/free-icons/notepad" title="notepad icons">Notepad icons
+                            created by Smashicons - Flaticon</a></li>
+                    </ul>
+                </div>
+                <div className={styles.column}>
+                    Extras:
+                    <ul>
+                        {/*<li><a href="https://www.flaticon.com/free-icons/notepad" title="notepad icons">Notepad icons*/}
+                        {/*    created by Smashicons - Flaticon</a></li>*/}
+                    </ul>
+                </div>
+            </footer>
+        </>
+    );
 }
 
 export default App;
